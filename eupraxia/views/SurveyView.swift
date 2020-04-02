00ac5efe0471c824cc1b854582ef221d897d0042
Thomas Guilleminot <guilleminot.thomas@gmail.com>
2020-04-02 14:51:53 +0200

    feat(survey-view): add general and food section

--- eupraxia/views/SurveyView.swift
+++ eupraxia/views/SurveyView.swift
@@ -9,8 +9,56 @@
 import SwiftUI
 
 struct SurveyView: View {
+
+    private var feelings = ["Very bad", "Bad", "Average", "Good", "Very good"]
+
+    @State private var selectedFeeling = -1
+    @State private var didEatBreakfast = false
+    @State private var breakfastFood = ""
+    @State private var didEatLunch = false
+    @State private var lunchFood = ""
+    @State private var didEatDinner = false
+    @State private var dinnerFood = ""
+
     var body: some View {
-        Text("Hello, World!")
+        NavigationView {
+            Form {
+                Section(header: Text("General")) {
+                    Picker(selection: $selectedFeeling, label: Text("How did you feel")) {
+                        ForEach(0..<self.feelings.count) {
+                            Text(self.feelings[$0])
+                        }
+                    }
+                }
+
+                Section(header: Text("Food")) {
+                    Toggle(isOn: $didEatBreakfast.animation()) {
+                        Text("Did you eat at breakfast ?")
+                    }
+                    if self.didEatBreakfast {
+                        TextField("What did you eat at breakfast", text: $breakfastFood)
+                    }
+
+                    Toggle(isOn: $didEatLunch.animation()) {
+                        Text("Did you eat at lunch ?")
+                    }
+                    if self.didEatLunch {
+                        TextField("What did you eat at lunch", text: $lunchFood)
+                    }
+
+                    Toggle(isOn: $didEatDinner.animation()) {
+                        Text("Did you eat at dinner ?")
+                    }
+                    if self.didEatDinner {
+                        TextField("What did you eat at dinner", text: $dinnerFood)
+                    }
+                }
+
+            }
+            .listStyle(GroupedListStyle())
+            .navigationBarTitle("How was your day ?")
+        }
+
     }
 }
 
