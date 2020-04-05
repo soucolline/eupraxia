344b376b343c591a4d7be1612bc5dc340b641778
Thomas Guilleminot <guilleminot.thomas@gmail.com>
2020-04-05 10:09:10 +0200

    feat(history-view-redesign): add custom fonts

---
+++ eupraxia/utils/Constants.swift
@@ -0,0 +1,17 @@
+//
+//  Constants.swift
+//  eupraxia
+//
+//  Created by Thomas Guilleminot on 05/04/2020.
+//  Copyright © 2020 Thomas Guilleminot. All rights reserved.
+//
+
+import Foundation
+
+struct K {
+    struct Font {
+        static let openSansRegular = "OpenSans-Regular"
+        static let openSansSemiBold = "OpenSans-SemiBold"
+        static let openSansBold = "OpenSans-Bold"
+    }
+}
