a3e76151ef6c96aa3e0018f634574a61b051e727
Thomas Guilleminot <guilleminot.thomas@gmail.com>
2020-04-05 11:35:47 +0200

    feat(history-view-redesign): add header

--- eupraxia/views/HistoryView.swift
+++ eupraxia/views/HistoryView.swift
@@ -17,35 +17,41 @@ struct HistoryView: View {
     ) var surveys: FetchedResults<Survey>
 
     var body: some View {
-        NavigationView {
-            List {
-                ForEach(surveys, id: \.id) { (survey: Survey) in
-                    ZStack {
-                        HistoryRow(viewModel: HistoryRowViewModel(with: survey))
-                        NavigationLink(destination: DetailsView(viewModel: DetailsViewModel(with: survey))) {
-                            EmptyView()
+        ZStack(alignment: .top) {
+            Image("history_background")
+                .offset(x: 0, y: -80)
+
+            ScrollView {
+                VStack(alignment: .leading) {
+                    Text("History")
+                        .font(Font.custom(K.Font.openSansBold, size: 35))
+                        .foregroundColor(Color.darkPink)
+                        .padding(.top, 72)
+                        .padding(.leading, 42)
+
+                    Text("You had no stomach pains for 28 days straight.")
+                        .font(Font.custom(K.Font.openSansSemiBold, size: 16))
+                        .foregroundColor(Color.darkPink)
+                        .padding(.top, 22)
+                        .padding(.leading, 42)
+
+                    VStack {
+                        ForEach(surveys, id: \.id) { (survey: Survey) in
+                            ZStack {
+                                HistoryRow(viewModel: HistoryRowViewModel(with: survey))
+                                NavigationLink(destination: DetailsView(viewModel: DetailsViewModel(with: survey))) {
+                                    EmptyView()
+                                }
+                            }
                         }
                     }
+                    .padding(.top, 30)
                 }
-                .onDelete(perform: delete)
+
+
             }
-            .navigationBarTitle("History")
-            .navigationBarItems(trailing: EditButton())
             .onAppear { UITableView.appearance().separatorStyle = .none }
             .onDisappear { UITableView.appearance().separatorStyle = .singleLine }
-        }
-    }
-
-    func delete(at offset: IndexSet) {
-        for index in offset {
-            let survey = self.surveys[index]
-            self.context.delete(survey)
-        }
-
-        do {
-            try self.context.save()
-        } catch {
-            fatalError("couldn't save context after deletion")
         }
     }
 }
